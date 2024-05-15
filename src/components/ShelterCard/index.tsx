ca7db7821afa3ce3394c83aa70527c0975b179c6
moraiskaua <moraiskaua03@gmail.com>
2024-05-15 18:53:16 -0300

    feat: open shelter details

--- src/components/ShelterCard/index.tsx
+++ src/components/ShelterCard/index.tsx
@@ -1,10 +1,11 @@
-import { View, Text, FlatList, Image } from 'react-native';
+import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native';
 import { Shelter } from '../../entities/Shelter';
 import Button from '../Button';
 import Icon from 'react-native-vector-icons/Feather';
 import { useShelterCardController } from './useShelterCardController';
 import ContactShelterModal from '../ContactShelterModal';
 import { useAnimalShelterController } from '../../screens/AnimalShelterScreen/useAnimalShelterController';
+import { useContactShelterModalController } from '../ContactShelterModal/useContactShelterModalController';
 
 interface ShelterCardProps {
   shelters: Shelter[];
@@ -19,6 +20,7 @@ const ShelterCard: React.FC<ShelterCardProps> = ({ shelters }) => {
     handlePressContact,
   } = useShelterCardController();
   const { handleOpenLocation } = useAnimalShelterController();
+  const { handleOpenPhone } = useContactShelterModalController();
 
   return (
     <>
@@ -39,42 +41,44 @@ const ShelterCard: React.FC<ShelterCardProps> = ({ shelters }) => {
           margin: 18,
         }}
         renderItem={({ item }) => (
-          <View className="bg-white p-4 rounded-2xl">
-            <Image
-              source={{ uri: item.images[0] }}
-              className="h-[180px] rounded-t-2xl object-cover"
-            />
+          <TouchableOpacity onPress={() => handlePressContact(item.id)}>
+            <View className="bg-white p-4 rounded-2xl">
+              <Image
+                source={{ uri: item.images[0] }}
+                className="h-[180px] rounded-t-2xl object-cover"
+              />
 
-            <View className="flex-row justify-between items-center mt-4">
-              <Text className="text-black font-bold">{item.name}</Text>
-            </View>
+              <View className="flex-row justify-between items-center mt-4">
+                <Text className="text-black font-bold">{item.name}</Text>
+              </View>
 
-            <View className="flex-row items-center justify-between mt-2.5">
-              <View className="flex-row items-center gap-1">
-                <Icon name="map-pin" size={14} />
-                <Text className="text-xs max-w-xs">{item.address}</Text>
+              <View className="flex-row items-center justify-between mt-2.5">
+                <View className="flex-row items-center gap-1">
+                  <Icon name="map-pin" size={14} />
+                  <Text className="text-xs max-w-xs">{item.address}</Text>
+                </View>
+              </View>
+
+              <View className="flex-row mt-3.5 space-x-3">
+                <View className="flex-1">
+                  <Button
+                    variant="primary"
+                    onPress={() => handleOpenLocation(item.address)}
+                  >
+                    <Icon name="map" size={18} />
+                  </Button>
+                </View>
+                <View className="flex-1">
+                  <Button
+                    variant="secondary"
+                    onPress={() => handleOpenPhone(item.phone)}
+                  >
+                    <Icon name="phone" size={20} />
+                  </Button>
+                </View>
               </View>
             </View>
-
-            <View className="flex-row mt-3.5 space-x-3">
-              <View className="flex-1">
-                <Button
-                  variant="primary"
-                  onPress={() => handleOpenLocation(item.address)}
-                >
-                  <Icon name="map" size={18} />
-                </Button>
-              </View>
-              <View className="flex-1">
-                <Button
-                  variant="secondary"
-                  onPress={() => handlePressContact(item.id)}
-                >
-                  <Icon name="phone" size={20} />
-                </Button>
-              </View>
-            </View>
-          </View>
+          </TouchableOpacity>
         )}
       />
     </>
