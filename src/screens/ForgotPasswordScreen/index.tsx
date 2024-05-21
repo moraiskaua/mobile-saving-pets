e139f2a66e7aaf07ef9f66e27f5586082873db86
D0uglasSantos <douglasas410@gmail.com>
2024-05-21 09:22:26 -0300

    feat: creating page forgot password

--- src/screens/ForgotPasswordScreen/index.tsx
+++ src/screens/ForgotPasswordScreen/index.tsx
@@ -3,26 +3,47 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 
 import Button from '../../components/Button';
 import { useForgotPasswordController } from './useForgotPasswordController';
+import { Controller } from 'react-hook-form';
+import Input from '../../components/Input';
 
 interface ForgotPasswordScreenProps {}
 
 const ForgotPasswordScreen: React.FC<ForgotPasswordScreenProps> = ({}) => {
-  const { goBack } = useForgotPasswordController();
+  const { goBack, control, errors, setValue, handleSubmit, onSubmit } =
+    useForgotPasswordController();
   return (
-    <SafeAreaView>
-      <View className="items-center justify-center pt-5">
+    <SafeAreaView className="flex-1 items-center">
+      <View className="items-center justify-center pt-5 mb-20">
         <Image
           source={require('../../assets/filled-logo.png')}
           className="h-16"
           resizeMode="contain"
         />
       </View>
-      <View className="h-full items-center">
-        <View className="w-full p-7" style={{ gap: 14 }}>
-          <Button variant="primary">Enviar código</Button>
-          <View className="flex-row justify-center items-center">
+      <View className="w-full p-4" style={{ gap: 20 }}>
+        <Text className="text-3xl font-bold text-gray-700 text-center">
+          Esqueci a senha
+        </Text>
+        <Controller
+          control={control}
+          name="email"
+          render={({ field }) => (
+            <Input
+              placeholder="Email"
+              autoCapitalize="none"
+              value={field.value}
+              onChangeText={value => setValue('email', value)}
+              error={errors.email?.message}
+            />
+          )}
+        />
+        <View className="w-full" style={{ gap: 14 }}>
+          <Button variant="primary" onPress={handleSubmit(onSubmit)}>
+            Enviar código
+          </Button>
+          <View className="flex items-center">
             <TouchableOpacity onPress={() => goBack()}>
-              <Text className="text-black/50">Voltar</Text>
+              <Text className="text-black/50">Voltar </Text>
             </TouchableOpacity>
           </View>
         </View>
